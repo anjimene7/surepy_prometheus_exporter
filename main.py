f0be069fd94a39590f6883bc46e7495aec8a48f0
Antonio Jimenez Gonzalez <anjimenewebmaster@hotmail.com>
2023-11-16 09:07:51 +0100

    Updated exporter code, POC

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import asyncio
 import logging
 from os import environ
 import time
-from typing import List
+from typing import List, Dict
 from surepy import Surepy, EntityType
-from surepy.entities import SurepyEntity
 from surepy.exceptions import SurePetcareConnectionError
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server
+from prometheus_client.core import GaugeMetricFamily, REGISTRY
 from sys import stdout
 
 # Define logger
@@ -18,68 +18,59 @@ consoleHandler = logging.StreamHandler(stdout) #set streamhandler to stdout
 consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
 
-class TimestampedGauge(Gauge):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
+class TimestampedGauge2(object):
+    def __init__(self):
+        pass
     def collect(self):
-        metrics = super().collect()
-        for metric in metrics:
-            samples = []
-            for sample in metric.samples:
-                timestamp = sample.labels.pop("timestamp", None)
-                sample_with_timestamp = type(sample)(sample.name, sample.labels, sample.value, timestamp, sample.exemplar)
-                samples.append(sample_with_timestamp)
-            metric.samples = samples
-        return metrics
+        logger.info(f"Collecting data")
+        data = extract_data()
+        cat_feed = GaugeMetricFamily(name='surepy_pets_feeding', documentation='Pets feeding status', labels=["pet", "household_id"])
+        feeder_food_status = GaugeMetricFamily('surepy_bowls_food', 'Bowls food status', labels=["pet", "household_id"])
+        feeder_battery_status = GaugeMetricFamily('surepy_bowls_battery', 'Bowls battery status', labels=["pet", "household_id"])
+        for k, v in data.items():
+            for el in v:
+                if "feed" == k:
+                    cat_feed.add_metric([str(el[0]), str(el[1])], str(el[2]), str(el[3]))
+                    yield cat_feed
+                elif "bowl" == k:
+                    feeder_food_status.add_metric([str(el[0]), str(el[1])], str(el[2]))
+                    feeder_battery_status.add_metric([str(el[0]), str(el[1])], str(el[3]))
+                    yield feeder_battery_status
+                    yield feeder_food_status
 
 
-def set_value_with_timestamp(metric, labels, value, timestamp):
-    labels["timestamp"] = timestamp
-    metric.labels(**labels).set(value)
 
-def extract_data(data: List[SurepyEntity]):
-    pets = [v for k, v in data.items() if v.type == EntityType.PET]
-    feeders = [v for k, v in data.items() if v.type == EntityType.FEEDER]
-    logger.debug(f"Got {len(pets)} pets and {len(feeders)} feeders.")
-    for pet in pets:
-        last_feeding_time = pet.feeding.at.timestamp()
-        cat_feed.labels(pet.name, pet.household_id, last_feeding_time).set(pet.feeding.change[0])
-        set_value_with_timestamp(cat_feed_ts, {"pet": pet.name, "household_id": pet.household_id, "timestamp": last_feeding_time}, pet.feeding.change[0],
-                                 last_feeding_time)
-    for feeder in feeders:
-        feeder_food_status.labels(feeder.name, feeder.household_id).set(feeder.total_weight)
-        feeder_battery_status.labels(feeder.name, feeder.household_id).set(feeder.battery_level)
+def extract_data() -> Dict[str, List]:
+    result = {}
+    count = 0
+    retry_count = 7
+    try:
+        data = asyncio.run(surepy.get_entities())
+    except SurePetcareConnectionError:
+        logger.error(f"Error connecting to API, will retry ({count}/{retry_count})")
+        if count >= retry_count:
+            raise ConnectionError(f"Could not get connection to API after {count} retries. Exiting...")
+        time.sleep(600)
+        count += 1
+    else:
+        pets = [v for k, v in data.items() if v.type == EntityType.PET]
+        feeders = [v for k, v in data.items() if v.type == EntityType.FEEDER]
+        logger.debug(f"Got {len(pets)} pets and {len(feeders)} feeders.")
+        for pet in pets:
+            last_feeding_time = pet.feeding.at.timestamp()
+            result.setdefault('feed', []).append([pet.name, pet.household_id, pet.feeding.change[0], last_feeding_time])
+        for feeder in feeders:
+            result.setdefault('bowl', []).append([feeder.name, feeder.household_id, feeder.total_weight, feeder.battery_level])
+    return result
 
 
 if __name__ == '__main__':
     logger.info("Starting script")
     surepy = Surepy(auth_token=environ.get("SUREPY_TOKEN"))
-    cat_feed = Gauge('surepy_pets_feeding', 'Pets feeding status', ["pet", "household_id", "eat_time"])
-    cat_feed_ts = TimestampedGauge('surepy_pets_feeding_ts', 'Pets feeding status', ["pet", "household_id", "timestamp"])
-    feeder_food_status = Gauge('surepy_bowls_food', 'Bowls food status', ["name", "household_id"])
-    feeder_battery_status = Gauge('surepy_bowls_battery', 'Bowls battery status', ["name", "household_id"])
     start_http_server(9000)
-    time_interval = environ.get("SUREPY_INTERVAL")
-    retry_count = 7
-    if time_interval is None:
-        time_interval = 5
-    count = 0
+    REGISTRY.register(TimestampedGauge2())
     while True:
-        logger.info("Extracting data from API")
-        try:
-            data = asyncio.run(surepy.get_entities())
-        except SurePetcareConnectionError:
-            logger.error(f"Error connecting to API, will retry ({count}/{retry_count})")
-            if count >= retry_count:
-                logger.error(f"Could not get connection to API after {count} retries. Exitting...")
-                break
-            time.sleep(600)
-            count += 1
-        else:
-            count = 0
-            extract_data(data)
-            cat_feed_ts.collect()
-        time.sleep(int(time_interval))
+        time.sleep(1)
+
 
 
