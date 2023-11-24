0cc52edf27abfd8feb6496702cde9da179dc16d8
Antonio Jimenez Gonzalez <anjimenewebmaster@hotmail.com>
2023-11-24 15:35:19 +0100

    Updated main script

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from sys import stdout
 # Define logger
 logger = logging.getLogger('surepy_prometheus_exporter')
 
-logger.setLevel(logging.INFO) # set logger level
+logger.setLevel(logging.DEBUG) # set logger level
 logFormatter = logging.Formatter("%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s")
 consoleHandler = logging.StreamHandler(stdout) #set streamhandler to stdout
 consoleHandler.setFormatter(logFormatter)
@@ -20,30 +20,31 @@ logger.addHandler(consoleHandler)
 
 class TimestampedGauge2(object):
     def __init__(self):
-        pass
+        self.cat_feed = GaugeMetricFamily(name='surepy_pets_feeding', documentation='Pets feeding status', labels=["pet", "household_id"])
+        self.cat_feed_immediate = GaugeMetricFamily(name='surepy_pets_feeding_instant', documentation='Pets feeding status (last)', labels=["pet", "household_id"])
+        self.feeder_food_status = GaugeMetricFamily('surepy_bowls_food', 'Bowls food status', labels=["pet", "household_id"])
+        self.feeder_battery_status = GaugeMetricFamily('surepy_bowls_battery', 'Bowls battery status', labels=["pet", "household_id"])
+
     def collect(self):
+        surepy = Surepy(auth_token=environ.get("SUREPY_TOKEN"))
         logger.info(f"Collecting data")
-        data = extract_data()
-        cat_feed = GaugeMetricFamily(name='surepy_pets_feeding', documentation='Pets feeding status', labels=["pet", "household_id"])
-        cat_feed_immediate = GaugeMetricFamily(name='surepy_pets_feeding_instant', documentation='Pets feeding status (last)', labels=["pet", "household_id"])
-        feeder_food_status = GaugeMetricFamily('surepy_bowls_food', 'Bowls food status', labels=["pet", "household_id"])
-        feeder_battery_status = GaugeMetricFamily('surepy_bowls_battery', 'Bowls battery status', labels=["pet", "household_id"])
+        data = extract_data(surepy)
         for k, v in data.items():
             for el in v:
                 if "feed" == k:
-                    cat_feed.add_metric([str(el[0]), str(el[1])], str(el[2]), str(el[3]))
-                    cat_feed_immediate.add_metric([str(el[0]), str(el[1])], str(el[2]))
-                    yield cat_feed
-                    yield cat_feed_immediate
+                    self.cat_feed.add_metric([str(el[0]), str(el[1])], str(el[2]), str(el[3]))
+                    self.cat_feed_immediate.add_metric([str(el[0]), str(el[1])], str(el[2]))
+                    yield self.cat_feed
+                    yield self.cat_feed_immediate
                 elif "bowl" == k:
-                    feeder_food_status.add_metric([str(el[0]), str(el[1])], str(el[2]))
-                    feeder_battery_status.add_metric([str(el[0]), str(el[1])], str(el[3]))
-                    yield feeder_battery_status
-                    yield feeder_food_status
+                    self.feeder_food_status.add_metric([str(el[0]), str(el[1])], str(el[2]))
+                    self.feeder_battery_status.add_metric([str(el[0]), str(el[1])], str(el[3]))
+                    yield self.feeder_battery_status
+                    yield self.feeder_food_status
 
 
 
-def extract_data() -> Dict[str, List]:
+def extract_data(surepy) -> Dict[str, List]:
     result = {}
     count = 0
     retry_count = 7
@@ -61,6 +62,7 @@ def extract_data() -> Dict[str, List]:
         logger.debug(f"Got {len(pets)} pets and {len(feeders)} feeders.")
         for pet in pets:
             last_feeding_time = pet.feeding.at.timestamp()
+            logger.debug(f"Last feeding time ({pet.name}): {last_feeding_time} -> {pet.feeding.change[0]}")
             result.setdefault('feed', []).append([pet.name, pet.household_id, pet.feeding.change[0], last_feeding_time])
         for feeder in feeders:
             result.setdefault('bowl', []).append([feeder.name, feeder.household_id, feeder.total_weight, feeder.battery_level])
@@ -69,7 +71,6 @@ def extract_data() -> Dict[str, List]:
 
 if __name__ == '__main__':
     logger.info("Starting script")
-    surepy = Surepy(auth_token=environ.get("SUREPY_TOKEN"))
     start_http_server(9000)
     REGISTRY.register(TimestampedGauge2())
     while True:
