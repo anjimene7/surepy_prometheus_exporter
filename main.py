0ff320dce4a427fb87ee5ce123f6eb5879ecf800
Antonio Jimenez Gonzalez <tonijimenez777@gmail.com>
2023-12-01 15:06:30 +0100

    Added backfill functionality

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 import logging
+import os.path
 from os import environ
-from datetime import datetime
+import csv
+from datetime import datetime, timezone
 import time
 from typing import List, Dict
 from surepy import Surepy, EntityType
@@ -37,7 +39,8 @@ consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
 
 household_status_metric = Gauge('surepy_household_status', 'Status of the SurePet household (online or offline)', labelnames=["serial", "name", "household_id"])
-pet_food_metric = TimestampedGauge(name='surepy_pet_food', documentation='Amount of food eaten by the pet', labelnames=["name", "household_id", "photo_url", "timestamp"])
+#pet_food_metric = TimestampedGauge(name='surepy_pet_food', documentation='Amount of food eaten by the pet', labelnames=["name", "household_id", "photo_url", "timestamp"])
+pet_food_metric = Gauge(name='surepy_pet_food', documentation='Amount of food eaten by the pet', labelnames=["name", "household_id", "photo_url"])
 feeder_food_metric = Gauge('surepy_bowls_food', 'Bowls food status', labelnames=["name", "household_id", "serial"])
 feeder_battery_metric = Gauge('surepy_bowls_battery', 'Bowls battery status', labelnames=["name", "household_id", "serial"])
 
@@ -99,24 +102,44 @@ def set_metrics(output_household, output_pets, output_feeder_battery, output_fee
     for i in output_household:
         household_status_metric.labels(*i['labels'].values()).set(i['value'])
     for i in output_pets:
-        timestruct = time.strptime(i['ts'], "%Y-%m-%dT%H:%M:%S%z")
-        set_value_with_timestamp(pet_food_metric, i['labels'], i['value'], int(time.mktime(timestruct)))
+        datetime_ts = datetime.strptime(i['ts'], "%Y-%m-%dT%H:%M:%S%z")
+        #set_value_with_timestamp(pet_food_metric, i['labels'], i['value'], int(time.mktime(datetime_ts)))
+        if datetime_ts > last_scrape:
+            pet_food_metric.labels(*i['labels'].values()).set(i['value'])
     for i in output_feeder_battery:
         feeder_battery_metric.labels(*i['labels'].values()).set(i['value'])
     for i in output_feeder_food:
         feeder_food_metric.labels(*i['labels'].values()).set(i['value'])
 
 
+def generate_csv_backfill(data):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backfill.csv'), 'w', newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(['name', 'household_id', 'photo_url', 'value', 'timestamp'])
+        for row in data:
+            writer.writerow([*row['labels'].values()]+[row['value'], row['ts']])
+    cmd = f"curl -d @backfill.csv http://localhost:8428/api/v1/import/csv?format=1:label:name,2:label:household_id,3:label:photo_url,4:metric,5:time:rfc3339"
+    # curl -d "MSFT,3.21,1.67,NASDAQ" 'http://localhost:8428/api/v1/import/csv?format=2:metric:ask,3:metric:bid,1:label:ticker,4:label:market'
+    logger.info(f"Command to backfill history: {cmd}")
+
+
+
 if __name__ == '__main__':
     logger.info("Starting script")
     start_http_server(9000)
+    scrape_time = 60
+    last_scrape = datetime.now(timezone.utc)
+    initial_run = True
     while True:
         surepy = Surepy(auth_token=environ.get("SUREPY_TOKEN"))
         output_household, output_pets, output_feeder_battery, output_feeder_food = extract_data(surepy)
-        logger.info(f"Extracted metrics: pet food: {len(output_pets)}")
+        generate_csv_backfill(output_pets)
+        logger.info(f"Extracted metrics: pet food: {len(output_pets)}. Last scrape : {last_scrape}")
+        last_scrape = datetime.now(timezone.utc)
         logger.debug(f"Extracted metrics: feeder battery: {output_feeder_battery}, feeder food: {output_feeder_food}, household: {output_household}, last pet timestamp: {output_pets[-1]}")
         set_metrics(output_household, output_pets, output_feeder_battery, output_feeder_food)
-        time.sleep(30)
+        time.sleep(scrape_time)
+        initial_run = False
 
 
 
