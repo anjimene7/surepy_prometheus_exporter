c3df706c237e79eb53ae428f166c09ae8bb57364
Antonio Jimenez Gonzalez <tonijimenez777@gmail.com>
2023-12-02 12:23:11 +0100

    Refactored backfill functionality with bind-mount

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import asyncio
 import logging
 import os.path
+import traceback
 from os import environ
 import csv
+import base64
+import requests
 from datetime import datetime, timezone
 import time
 from typing import List, Dict
@@ -12,38 +15,19 @@ from prometheus_client import start_http_server, Gauge
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 from sys import stdout
 
-
-class TimestampedGauge(Gauge):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def collect(self):
-        metrics = super().collect()
-        for metric in metrics:
-            samples = []
-            for sample in metric.samples:
-                timestamp = sample.labels.pop("timestamp", None)
-                sample_with_timestamp = type(sample)(sample.name, sample.labels,
-                                                     sample.value, timestamp, sample.exemplar)
-                samples.append(sample_with_timestamp)
-            metric.samples = samples
-        return metrics
-
 # Define logger
 logger = logging.getLogger('surepy_prometheus_exporter')
 
-logger.setLevel(logging.DEBUG) # set logger level
+logger.setLevel(logging.INFO) # set logger level
 logFormatter = logging.Formatter("%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s")
 consoleHandler = logging.StreamHandler(stdout) #set streamhandler to stdout
 consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
 
 household_status_metric = Gauge('surepy_household_status', 'Status of the SurePet household (online or offline)', labelnames=["serial", "name", "household_id"])
-#pet_food_metric = TimestampedGauge(name='surepy_pet_food', documentation='Amount of food eaten by the pet', labelnames=["name", "household_id", "photo_url", "timestamp"])
-pet_food_metric = Gauge(name='surepy_pet_food', documentation='Amount of food eaten by the pet', labelnames=["name", "household_id", "photo_url"])
+#pet_food_metric = Gauge(name='surepy_pet_food', documentation='Amount of food eaten by the pet', labelnames=["name", "household_id", "photo_url"])
 feeder_food_metric = Gauge('surepy_bowls_food', 'Bowls food status', labelnames=["name", "household_id", "serial"])
 feeder_battery_metric = Gauge('surepy_bowls_battery', 'Bowls battery status', labelnames=["name", "household_id", "serial"])
-
 
 
 def set_value_with_timestamp(metric, labels, value, timestamp):
@@ -106,15 +90,21 @@ def extract_data(surepy) -> Dict[str, List]:
 def set_metrics(output_household, output_pets, output_feeder_battery, output_feeder_food) -> None:
     for i in output_household:
         household_status_metric.labels(*i['labels'].values()).set(i['value'])
-    for i in output_pets:
-        datetime_ts = datetime.strptime(i['ts'], "%Y-%m-%dT%H:%M:%S%z")
-        #set_value_with_timestamp(pet_food_metric, i['labels'], i['value'], int(time.mktime(datetime_ts)))
-        if datetime_ts > last_scrape:
-            pet_food_metric.labels(*i['labels'].values()).set(i['value'])
     for i in output_feeder_battery:
         feeder_battery_metric.labels(*i['labels'].values()).set(i['value'])
     for i in output_feeder_food:
         feeder_food_metric.labels(*i['labels'].values()).set(i['value'])
+    for i in output_pets:
+        datetime_ts = datetime.strptime(i['ts'], "%Y-%m-%dT%H:%M:%S%z")
+        datetime_ts_ms = int(datetime_ts.timestamp() * 1000)
+        with open(already_written, 'r+') as f:
+            if f"{datetime_ts_ms},{i['value']}" not in f.read():
+                url = pushgateway_url.replace('prometheus', f'prometheus?timestamp={datetime_ts_ms}')
+                payload = f'surepy_pet_food{{monitor="my-project", name="{i["labels"]["name"]}", household_id="{i["labels"]["household_id"]}", photo_url="{i["labels"]["photo_url"]}"}} {i["value"]}'
+                #r = requests.post(url, payload)
+                #r.raise_for_status()
+                f.write(f"{datetime_ts_ms},{i['value']}\n")
+
 
 
 def generate_csv_backfill(pet_data, feeder_data):
@@ -122,14 +112,14 @@ def generate_csv_backfill(pet_data, feeder_data):
         writer = csv.writer(f, delimiter=',')
         for row in pet_data:
             writer.writerow([*row['labels'].values()]+[row['value'], (datetime.strptime(row['ts'], "%Y-%m-%dT%H:%M:%S%z")).strftime('%s')])
-    cmd_pet = f"curl --data-binary @backfill_pet.csv http://192.168.1.80:8428/api/v1/import/csv?format=1:label:name,2:label:household_id,3:label:photo_url,4:metric:surepy_pet_food,5:time:unix_s"
+    cmd_pet = f"curl --data-binary @backfill_pet.csv http://{hostname}:{port}/api/v1/import/csv?format=1:label:name,2:label:household_id,3:label:photo_url,4:metric:surepy_pet_food,5:time:unix_s"
 
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backfill_feeder.csv'), 'w', newline='') as f:
         writer = csv.writer(f, delimiter=',')
         for row in feeder_data:
             writer.writerow([*row['labels'].values()] + [row['value'], (datetime.strptime(row['ts'], "%Y-%m-%dT%H:%M:%S%z")).strftime('%s')])
 
-    cmd_feeder = f"curl --data-binary @backfill_feeder.csv http://192.168.1.80:8428/api/v1/import/csv?format=1:label:name,2:label:household_id,3:label:serial,4:metric:surepy_bowls_food,5:time:unix_s"
+    cmd_feeder = f"curl --data-binary @backfill_feeder.csv http://{hostname}:{port}/api/v1/import/csv?format=1:label:name,2:label:household_id,3:label:serial,4:metric:surepy_bowls_food,5:time:unix_s"
 
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backfill.sh'), 'w', newline='') as f:
         f.write(cmd_pet+'\n')
@@ -142,19 +132,27 @@ if __name__ == '__main__':
     logger.info("Starting script")
     start_http_server(9000)
     scrape_time = 60
-    last_scrape = datetime.now(timezone.utc)
-    initial_run = True
+    hostname = '192.168.1.80'
+    port = 8428
+    pushgateway_url = url = f"http://{hostname}:{port}/api/v1/import/prometheus"
+    already_written = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.already_written.txt')
+    if not os.path.exists(already_written):
+        open(already_written, 'w').close()
+    #initial_run = True
     while True:
         surepy = Surepy(auth_token=environ.get("SUREPY_TOKEN"))
         output_household, output_pets, output_feeder_history, output_feeder_battery, output_feeder_food = extract_data(surepy)
-        if initial_run:
-            generate_csv_backfill(output_pets, output_feeder_history)
-        logger.info(f"Extracted metrics: pet food: {len(output_pets)}. Last scrape : {last_scrape}")
-        last_scrape = datetime.now(timezone.utc)
+        #if initial_run:
+        #    generate_csv_backfill(output_pets, output_feeder_history)
+        logger.info(f"Extracted metrics: pet food: {len(output_pets)}")
         logger.debug(f"Extracted metrics: feeder battery: {output_feeder_battery}, feeder food: {output_feeder_food}, household: {output_household}, last pet timestamp: {output_pets[-1]}")
-        set_metrics(output_household, output_pets, output_feeder_battery, output_feeder_food)
+        try:
+            set_metrics(output_household, output_pets, output_feeder_battery, output_feeder_food)
+        except Exception as e:
+            traceback.print_exc()
+            logger.error(f"Issue setting metric: {e}")
         time.sleep(scrape_time)
-        initial_run = False
+        #initial_run = False
 
 
 
